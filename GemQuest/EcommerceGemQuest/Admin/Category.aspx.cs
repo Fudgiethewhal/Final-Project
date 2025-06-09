16566e5a154220b4928cebd57bb0fb664325fd42
Bryseida Hernandez <bryseida.hernandez@yahoo.com>
2025-06-08 17:53:33 -0700

    added alert

--- GemQuest/EcommerceGemQuest/Admin/Category.aspx.cs
+++ GemQuest/EcommerceGemQuest/Admin/Category.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
@@ -26,8 +27,58 @@ namespace EcommerceGemQuest.Admin
             string actionName = string.Empty, imagePath = string.Empty, fileExtension = string.Empty;
             bool isValidToExecute = false;
             int categoryId = Convert.ToInt32(hfCategoryId.Value);
-            con = new SqlConnection();
+            con = new SqlConnection(Utils.getConnection());
+            cmd = new SqlCommand("Category_Crud", con);
+            cmd.Parameters.AddWithValue("@Action", categoryId == 0 ? "INSERT" : "UPDATE");
+            cmd.Parameters.AddWithValue("@CategoryId", categoryId);
+            cmd.Parameters.AddWithValue("@CategoryName", txtCategoryName.Text.Trim());
+            cmd.Parameters.AddWithValue("@IsActive", cbIsActive.Checked);
+            if (fuCategoryImage.HasFile)
+            {
+                if(Utils.isValidExtension(fuCategoryImage.FileName))
+                {
+                    string newImageName = Utils.getUniqueId();
+                    fileExtension = Path.GetExtension(fuCategoryImage.FileName);
+                    imagePath = "Images/Category/" + newImageName.ToString() + fileExtension;
+                    fuCategoryImage.PostedFile.SaveAs(Server.MapPath("~/Images/Category/") + newImageName.ToString() + fileExtension);
+                    cmd.Parameters.AddWithValue("@CategoryImageUrl", imagePath);
+                }
+                else
+                {
+                    lblMsg.Visible = false;
+                    lblMsg.Text = "Please select .jpg, .jpeg, or .png image";
+                    lblMsg.CssClass = "alert alert-danger";
+                }    
+            }
+            else
+            {
+                isValidToExecute = true;
 
+            }
+
+            if(isValidToExecute)
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                try
+                {
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    actionName = categoryId == 0 ? "inserted" : "Successful";
+                    lblMsg.Visible = true;
+                    lblMsg.Text = "Category" + actionName + "Successful!";
+                    lblMsg.CssClass = "alert alert-success";
+                }
+                catch (Exception ex)
+                {
+                    lblMsg.Visible = true;
+                    lblMsg.Text = "Error- " + ex.Message;
+                    lblMsg.CssClass = "alert alert-danger";
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
         }
 
         protected void btnClear_Click(object sender, EventArgs e)
